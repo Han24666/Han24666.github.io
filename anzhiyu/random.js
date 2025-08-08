5265204ed2fedeec37fe1d1dbcab16ad83e8a6c3
Han24666 <3624086548@qq.com>
2025-08-08 20:35:59 +0800

    Site updated: 2025-08-08 20:35:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/08/题解：P2679-NOIP-2015-提高组-子串/","2025/08/05/hello-world/"];function toRandomPost(){
+var posts=["2025/08/05/hello-world/","2025/08/08/题解：P2679-NOIP-2015-提高组-子串/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
